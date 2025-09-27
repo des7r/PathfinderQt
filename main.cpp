aa85560af1bc0e6140e52e84dec74206bdb12f6f
Ahmad <65721237+des7r@users.noreply.github.com>
2025-09-27 21:27:13 +0300

    Qt pathfinder project

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <QApplication>
 #include "mainwindow.h"
-
-// Точка входа в программу. Создаёт QApplication, главное окно и запускает цикл событий Qt.
+ 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
